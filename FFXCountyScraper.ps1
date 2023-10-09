9f47c9c1d5d07ce388635bae841cf00253a7867a
boothinator <boothinator@users.noreply.github.com>
2023-10-09 18:26:52 -0400

    Add remaining PA

--- FFXCountyScraper.ps1
+++ FFXCountyScraper.ps1
@@ -6,206 +6,395 @@
 
 $items = @(@{
     folder = "Policy Plan Amendments"
-    name = "2022-CW-2CP"
+    paNumber = "2022-CW-2CP"
     plusNumber = "PA-2022-00009"
     district = "Countywide"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/Countywide-policy-plan-update"
 },
 @{
     folder = "Manufactured Housing"
-    name = "2022-CW-1CP"
+    paNumber = "2022-CW-1CP"
     plusNumber = "PA-2022-00006"
     district = "Countywide"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/manufactured-housing"
 },
 @{
     folder = "Heritage Resources"
-    name = "2017-CW-4CP"
+    paNumber = "2017-CW-4CP"
     plusNumber = ""
     district = "Countywide"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/2017-heritage-resources-plan-update"
 },
 @{
     folder = "Public Facilities Policy Plan"
-    name = "2020-CW-1CP"
+    paNumber = "2020-CW-1CP"
     plusNumber = "PA-2020-00026"
     district = "Countywide"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/public-facilities-policy"
 },
 @{
     folder = "Public Facilities Plan Map"
-    name = "2013-CW-5CP"
+    paNumber = "2013-CW-5CP"
     plusNumber = "PA-2020-00003"
     district = "Countywide"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/public-facilities-plan-map"
 },
 @{
     folder = "Fairfax Center Area, Phase III, Core Area"
-    name = "2013-III-FC1 (C)"
+    paNumber = "2013-III-FC1 (C)"
     plusNumber = "PA-2020-00003"
     district = "Braddock"
     url = "https://www.fairfaxcounty.gov/planning-development/fairfax-center"
 },
 <#@{ # Can't find page
     folder = "Centreville Study"
-    name = "2022-III-BR1"
+    paNumber = "2022-III-BR1"
     plusNumber = "PA-2022-00008"
     district = "Sully"
     url = ""
 },#>
 @{
     folder = "Lorton Visioning"
-    name = "2021-IV-LP1"
+    paNumber = "2021-IV-LP1"
     plusNumber = "PA-2021-00011"
     district = "Mount Vernon"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/lorton-visioning"
 },
 @{
     folder = "Innovation Center TSA North Study"
-    name = "2023-III-1UP"
+    paNumber = "2023-III-1UP"
     plusNumber = "PA-2023-00002"
     district = "Dranesville"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/innovation-center-tsa-north"
 },
 @{
     folder = "Springfield TSA/CBC Study"
-    name = "2023-IV-1S"
+    paNumber = "2023-IV-1S"
     plusNumber = "PA-2023-00003"
     district = "Franconia"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/springfield-tsa-cbc"
 },
 @{
     folder = "Penn Daw CBC Land Unit C Study"
-    name = "2023-IV-1MV"
+    paNumber = "2023-IV-1MV"
     plusNumber = "PA-2023-00004"
     district = "Mount Vernon"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/penn-daw-cbc-luc"
 },
 @{
     folder = "Fair Lakes Study"
-    name = "2023-III-1FC"
+    paNumber = "2023-III-1FC"
     plusNumber = "PA-2023-00005"
     district = "Springfield"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/fair-lakes-study"
 },
 @{
     folder = "Franconia Triangle Study"
-    name = "2023-IV-2S"
+    paNumber = "2023-IV-2S"
     plusNumber = "PA-2023-00006"
     district = "Franconia"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/franconia-triangle"
 }<#, # Can't find URL
 @{
     folder = "Reston TSA Study"
-    name = "2023-III-6UP"
+    paNumber = "2023-III-6UP"
     plusNumber = "PA-2023-00006"
     district = "Franconia"
     url = ""
 }#>,
 @{
     folder = "Brookside Motel"
-    name = "2021-IV-MV1"
+    paNumber = "2021-IV-MV1"
     plusNumber = "PA-2021-00023"
     district = "Mount Vernon"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/brookside-motel"
 },
 @{
     folder = "Judicial Complex"
-    name = "2021-IV-MV1"
+    paNumber = "2021-IV-MV1"
     plusNumber = "PA-2022-00007"
     district = "Providence"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/judicial-complex"
 },
 @{
     folder = "Cornerstones"
-    name = "2022-III-FC1"
+    paNumber = "2022-III-FC1"
     plusNumber = "PA-2022-00003"
     district = "Sully"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/cornerstones"
 },
 @{
     folder = "Dranesville United Methodist Church"
-    name = "2023-III-2UP"
+    paNumber = "2023-III-2UP"
     plusNumber = "PA-2023-00007"
     district = "Dranesville"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/dranesville-umc"
 },
 @{
     folder = "Cityside Huntington"
-    name = "2023-IV-2MV"
+    paNumber = "2023-IV-2MV"
     plusNumber = "PA-2023-00008"
     district = "Mount Vernon"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/cityside-huntington"
 },
 @{
     folder = "AT_T Oakton"
-    name = "2023-II-1F"
+    paNumber = "2023-II-1F"
     plusNumber = "PA-2023-00009"
     district = "Providence"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/att-oakton"
 },
 @{
     folder = "Alliance Center"
-    name = "2023-I-1J"
+    paNumber = "2023-I-1J"
     plusNumber = "PA-2023-00008"
     district = "Providence"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/alliance-center"
 },
 @{
     folder = "Merrifield at Dunn Loring Station Apartments"
-    name = "2023-I-2J"
+    paNumber = "2023-I-2J"
     plusNumber = "PA-2023-00011"
     district = "Providence"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/merrifield-dls-apartments"
 },
 @{
     folder = "Sequoia Capital"
-    name = "2023-I-3J"
+    paNumber = "2023-I-3J"
     plusNumber = "PA-2023-00012"
     district = "Providence"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/sequoia-capital"
 },
 @{
     folder = "Agape House"
-    name = "2023-III-3UP"
+    paNumber = "2023-III-3UP"
     plusNumber = "PA-2023-00013"
     district = "Sully"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/agape-house"
 },
 @{
     folder = "Park Center"
-    name = " 2023-III-1BR"
+    paNumber = " 2023-III-1BR"
     plusNumber = "PA-2023-00014"
     district = "Sully"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/park-center"
 },
 @{
     folder = "Springfield Boulevard"
-    name = "2023-IV-6S"
+    paNumber = "2023-IV-6S"
     plusNumber = "PA-2023-00038"
     district = "Franconia"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/springfield-blvd"
 },
 @{
     folder = "Amherst Avenue"
-    name = "2023-IV-7S"
+    paNumber = "2023-IV-7S"
     plusNumber = "PA-2023-00039"
     district = "Franconia"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/amherst-ave"
 },
 @{
     folder = "Frontier Drive"
-    name = "2023-IV-8S"
+    paNumber = "2023-IV-8S"
     plusNumber = "PA-2023-00040"
     district = "Franconia"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/frontier-dr"
 },
 @{
     folder = "Inova-Springfield Center Drive"
-    name = "2023-IV-9S"
+    paNumber = "2023-IV-9S"
     plusNumber = "PA-2023-00041"
     district = "Franconia"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/inova-springfield-center-dr"
+},
+@{
+    folder = "6320 Grovedale Drive"
+    paNumber = "2023-IV-3S"
+    plusNumber = "PA-2023-00017"
+    district = "Franconia"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/6230-grovedale-dr"
+},
+@{
+    folder = "A_A Contracting"
+    paNumber = "2023-III-2BR"
+    plusNumber = "PA-2023-00027"
+    district = "Sully"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/aa-contracting"
+},
+@{
+    folder = "Boston Boulevard - Costco"
+    paNumber = "2023-IV-4S"
+    plusNumber = "PA-2023-00022"
+    district = "Mount Vernon"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/boston-blvd-costco"
+},
+@{
+    folder = "Briarwood Farm"
+    paNumber = "2023-II-1V"
+    plusNumber = "PA-2023-00024"
+    district = "Providence"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/briarwood-farm"
+},
+@{
+    folder = "Cavalier Club"
+    paNumber = "2023-I-2B"
+    plusNumber = "PA-2023-00019"
+    district = "Mason"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/cavalier-club"
+},
+@{
+    folder = "Church Street"
+    paNumber = "2023-I-1B"
+    plusNumber = "PA-2023-00018"
+    district = "Mason"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/church-st"
+},
+@{
+    folder = "Discovery Square"
+    paNumber = "2023-III-4UP"
+    plusNumber = "PA-2023-00028"
+    district = "Sully"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/discovery-square"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/elm-st-communities"
+    paNumber = "2023-II-1M"
+    folder = "Elm Street Communities"
+    plusNumber = "PA-2023-00016"
+    district = "Dranesville"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/fairfax-center-suj1-2"
+    paNumber = "2023-II-2F"
+    folder = "Fairfax Center Suburban Center Sub-Unit J1 J2 and Evergreen Investment Co. LLC"
+    plusNumber = "PA-2023-00015"
+    district = "Braddock"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/fair-oaks-business-park"
+    paNumber = "2023-III-2FC"
+    folder = "Fair Oaks Business Park"
+    plusNumber = "PA-2023-00025"
+    district = "Springfield"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/fairfax-ridge-rd"
+    paNumber = "2023-III-3FC"
+    folder = "Fairfax Ridge Road"
+    plusNumber = "PA-2023-00026"
+    district = ""
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/gallows-rd"
+    paNumber = "2023-I-1A"
+    folder = "Gallows Road"
+    plusNumber = "PA-2023-00020"
+    district = "Mason"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/lincoln-park"
+    paNumber = "2023-III-5UP"
+    folder = "Lincoln Park"
+    plusNumber = "PA-2023-00029"
+    district = "Sully"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/shoppes-lorton-valley"
+    paNumber = "2023-III-1P"
+    folder = "Shoppes of Lorton Valley"
+    plusNumber = "PA-2023-00021"
+    district = "Mount Vernon"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/8800-richmond-hwy"
+    paNumber = "2018-IV-MV2"
+    folder = "8800 Richmond Highway"
+    plusNumber = "PA-2020-00008"
+    district = "Mount Vernon"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/beacon-hybla"
+    paNumber = "2018-IV-MV3"
+    folder = "Beacon Groveton and Hybla Valley Gum Springs Areas"
+    plusNumber = "PA-2020-00009"
+    district = "Mount Vernon"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/brookfield"
+    paNumber = "2023-III-7UP"
+    folder = "Brookfield"
+    plusNumber = "PA-2023-00032"
+    district = "Hunter Mill"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/church-good-shepherd"
+    paNumber = "2023-II-3F"
+    folder = "Church of the Good Shepherd"
+    plusNumber = "PA-2023-00030"
+    district = "Braddock"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/engleside-trailer-park"
+    paNumber = "2021-IV-1MV"
+    folder = "Engleside Trailer Park Ray's Mobile Home Colony"
+    plusNumber = "PA-2021-00003"
+    district = "Mount Vernon"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/fallfax"
+    paNumber = "2023-I-5J"
+    folder = "Fallfax"
+    plusNumber = "PA-2023-00037"
+    district = "Providence"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/icf-building"
+    paNumber = "2023-II-2V"
+    folder = "ICF Building"
+    plusNumber = "PA-2023-00036"
+    district = "Providence"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/mt-vernon-highway"
+    paNumber = "2021-IV-2MV"
+    folder = "Mount Vernon Highway SNA"
+    plusNumber = "PA-2021-00004"
+    district = "Mount Vernon"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/pistones"
+    paNumber = "2023-I-4J"
+    folder = "Pistones"
+    plusNumber = "PA-2023-00035"
+    district = "Mason"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/rhc-sna-study"
+    paNumber = "2018-IV-MV6"
+    folder = "Richmond Highway Corridor - Suburban Neighborhood Areas Study"
+    plusNumber = "PA-2020-00010"
+    district = "Mount Vernon"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/rose-hill-shopping-ctr"
+    paNumber = "2023-IV-1RH"
+    folder = "Rose Hill Shopping Center"
+    plusNumber = "PA-2023-00033"
+    district = "Franconia"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/van-dorn-tsa"
+    paNumber = "2021-IV-1RH"
+    folder = "Van Dorn TSA Land Units D and E (part)"
+    plusNumber = "PA-2021-00008"
+    district = "Franconia"
+},
+@{
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/vine-st"
+    paNumber = "2023-IV-2RH"
+    folder = "Vine Street"
+    plusNumber = "PA-2023-00034"
+    district = "Franconia"
 }
 
 )
