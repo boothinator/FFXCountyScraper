0932a69799dcacdf520776a94b06ba96c913a7e2
boothinator <boothinator@users.noreply.github.com>
2023-10-09 10:46:32 -0400

    Add more items and fix bugs

--- FFXCountyScraper.ps1
+++ FFXCountyScraper.ps1
@@ -46,20 +46,13 @@ $items = @(@{
     district = "Braddock"
     url = "https://www.fairfaxcounty.gov/planning-development/fairfax-center"
 },
-@{
-    folder = "Fairfax Center Area, Phase III, Core Area"
-    name = "2013-III-FC1 (C)"
-    plusNumber = "PA-2020-00003"
-    district = "Braddock"
-    url = "https://www.fairfaxcounty.gov/planning-development/fairfax-center"
-},
-@{
+<#@{ # Can't find page
     folder = "Centreville Study"
     name = "2022-III-BR1"
     plusNumber = "PA-2022-00008"
     district = "Sully"
-    url = "https://www.fairfaxcounty.gov/planning-development/fairfax-center"
-},
+    url = ""
+},#>
 @{
     folder = "Lorton Visioning"
     name = "2021-IV-LP1"
@@ -115,15 +108,114 @@ $items = @(@{
     plusNumber = "PA-2021-00023"
     district = "Mount Vernon"
     url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/brookside-motel"
+},
+@{
+    folder = "Judicial Complex"
+    name = "2021-IV-MV1"
+    plusNumber = "PA-2022-00007"
+    district = "Providence"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/judicial-complex"
+},
+@{
+    folder = "Cornerstones"
+    name = "2022-III-FC1"
+    plusNumber = "PA-2022-00003"
+    district = "Sully"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/cornerstones"
+},
+@{
+    folder = "Dranesville United Methodist Church"
+    name = "2023-III-2UP"
+    plusNumber = "PA-2023-00007"
+    district = "Dranesville"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/dranesville-umc"
+},
+@{
+    folder = "Cityside Huntington"
+    name = "2023-IV-2MV"
+    plusNumber = "PA-2023-00008"
+    district = "Mount Vernon"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/cityside-huntington"
+},
+@{
+    folder = "AT_T Oakton"
+    name = "2023-II-1F"
+    plusNumber = "PA-2023-00009"
+    district = "Providence"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/att-oakton"
+},
+@{
+    folder = "Alliance Center"
+    name = "2023-I-1J"
+    plusNumber = "PA-2023-00008"
+    district = "Providence"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/alliance-center"
+},
+@{
+    folder = "Merrifield at Dunn Loring Station Apartments"
+    name = "2023-I-2J"
+    plusNumber = "PA-2023-00011"
+    district = "Providence"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/merrifield-dls-apartments"
+},
+@{
+    folder = "Sequoia Capital"
+    name = "2023-I-3J"
+    plusNumber = "PA-2023-00012"
+    district = "Providence"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/sequoia-capital"
+},
+@{
+    folder = "Agape House"
+    name = "2023-III-3UP"
+    plusNumber = "PA-2023-00013"
+    district = "Sully"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/agape-house"
+},
+@{
+    folder = "Park Center"
+    name = " 2023-III-1BR"
+    plusNumber = "PA-2023-00014"
+    district = "Sully"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/park-center"
+},
+@{
+    folder = "Springfield Boulevard"
+    name = "2023-IV-6S"
+    plusNumber = "PA-2023-00038"
+    district = "Franconia"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/springfield-blvd"
+},
+@{
+    folder = "Amherst Avenue"
+    name = "2023-IV-7S"
+    plusNumber = "PA-2023-00039"
+    district = "Franconia"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/amherst-ave"
+},
+@{
+    folder = "Frontier Drive"
+    name = "2023-IV-8S"
+    plusNumber = "PA-2023-00040"
+    district = "Franconia"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/frontier-dr"
+},
+@{
+    folder = "Inova-Springfield Center Drive"
+    name = "2023-IV-9S"
+    plusNumber = "PA-2023-00041"
+    district = "Franconia"
+    url = "https://www.fairfaxcounty.gov/planning-development/plan-amendments/inova-springfield-center-dr"
 }
+
 )
 
+$rootPath = $PSCommandPath | Split-Path -Parent
 $timestamp = Get-Date -Format o | ForEach-Object { $_ -replace ":", "." }
 $diffPath = "$($rootPath)/content-$($timestamp).txt"
 
 foreach ($item in $items)
 {
-    $rootPath = $PSCommandPath | Split-Path -Parent
     $folderPath = "$($rootPath)/$($item.folder)"
     $contentPath = "$($folderPath)/content.html"
     $timestamp = Get-Date -Format o | ForEach-Object { $_ -replace ":", "." }
